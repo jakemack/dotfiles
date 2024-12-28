b67663bd56490430c0c3f45c6c2a6b0b10aeb2c9
Jake Mack <jakemack@gmail.com>
2024-12-28 11:36:10 -0800

    Changes

--- omz_customizations/pyenv.zsh
+++ omz_customizations/pyenv.zsh
@@ -1,10 +1,10 @@
-## init according to man page
-#if (( $+commands[pyenv] ))
-#then
-#  _evalcache pyenv init --path
-#  _evalcache pyenv init -
-#  _evalcache pyenv virtualenv-init -
-##  eval "$(pyenv init --path)"
-##  eval "$(pyenv init -)"
-##  eval "$(pyenv virtualenv-init -)"
-#fi
+# init according to man page
+if (( $+commands[pyenv] ))
+then
+  _evalcache pyenv init --path
+  _evalcache pyenv init -
+  _evalcache pyenv virtualenv-init -
+#  eval "$(pyenv init --path)"
+#  eval "$(pyenv init -)"
+#  eval "$(pyenv virtualenv-init -)"
+fi
--- omz_customizations/rbenv.zsh
+++ omz_customizations/rbenv.zsh
@@ -1,6 +1,6 @@
 ## init according to man page
-#if (( $+commands[rbenv] ))
-#then
-#  _evalcache rbenv init -
-##  eval "$(rbenv init -)"
-#fi
+if (( $+commands[rbenv] ))
+then
+  _evalcache rbenv init -
+#  eval "$(rbenv init -)"
+fi
